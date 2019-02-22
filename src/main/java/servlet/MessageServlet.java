344e9e803fb061ef12d6081500b11260b98f90ae
Aghunik Manukyan <aghmanukyan97@gmail.com>
2019-02-22 15:51:51 +0300

    message

--- src/main/java/servlet/MessageServlet.java
+++ src/main/java/servlet/MessageServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = "/user/message")
-public class messageServlet extends HttpServlet {
+public class MessageServlet extends HttpServlet {
     private UserManager userManager = new UserManager();
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
